d994eb828dd3b3d244b5ef620bd3fdf2f158203a
Gribesh Dhakal <gribeshdhakal@gmail.com>
2020-07-20 20:57:07 +0545

    added while statements

--- webcam.py
+++ webcam.py
@@ -2,3 +2,9 @@ import cv2
 
 imgCapture = cv2.VideoCapture(0)
 result = True
+
+while(result):
+    ret, frame = imgCapture.read()
+    cv2.imwrite("photo.jpg", frame)
+    result = False
+    print("Image Captured")
